a02f3729e30443281dcb581af6b3e965fcd4682f
Sako Hartounian <sakohartounian@sakos-mbp.attlocal.net>
2017-11-28 21:04:49 -0800

    now tests worked react components.

--- test/components/Header.spec.js
+++ test/components/Header.spec.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import { Link } from 'react-router-dom';
-// import sinon from 'sinon';
 
 import Header from '../../src/client/components/Header';
 
-decribe('Header', () => {
+describe('Header', () => {
     it('contains a Link to the Home Page', () => {
         const wrapper = shallow(<Header/>);
-        
+
         expect(wrapper.contains(<Link to='/'>Home</Link>)).to.equal(true);
     });
 });
