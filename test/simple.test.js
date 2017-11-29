10fddfa4cfb7ee11895c97bbc769f7f9d37b07d9
Sako Hartounian <sakohartounian@Sakos-MacBook-Pro.local>
2017-11-28 17:27:20 -0800

    simple testing added and now command line tools now called from local project .bin.

---
+++ test/simple.test.js
@@ -0,0 +1,14 @@
+// const expect = require('chai').expect;
+import { expect } from 'chai';
+
+describe('Mocha works', () => {
+    it('1+1=2', () => {
+        expect(1+1).to.equal(2);
+    });
+});
+
+// describe('Home', () => {
+//     it('renders', () => {
+
+//     });
+// });
