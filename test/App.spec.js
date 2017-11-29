53ab0dc7a75b25b81ad1151a5340c71ed72420be
Sako Hartounian <sakohartounian@Sakos-MacBook-Pro.local>
2017-11-29 09:46:44 -0800

    App spec and pages folder for tests added.

---
+++ test/App.spec.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import { Link } from 'react-router-dom';
+import Header from '../src/client/components/Header';
+import Footer from '../src/client/components/Footer';
+import App from '../src/client/App';
+
+describe('<App/>', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<App/>);
+    });
+
+    it('should contain a <Header/>', () => {        
+        expect(wrapper.contains(<Header/>)).to.equal(true);
+    });
+
+    it('should contain a <Footer/>', () => {        
+        expect(wrapper.contains(<Footer/>)).to.equal(true);
+    });
+});
