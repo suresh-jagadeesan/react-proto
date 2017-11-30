4363c98e542e4f9a4e2a2c5deaa431b8b11813e7
Sako Hartounian <shartounian@DSC-ML-SHARTOUNIAN.local>
2017-11-30 10:23:46 -0800

    Actions creators simplified

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,9 +1,9 @@
 import { INCREMENT, DECREMENT } from './action-types';
 
-export const increment = () => dispatch => {
-    dispatch({ type: INCREMENT });
+export const increment = () => {
+    return { type: INCREMENT };
 };
 
-export const decrement = () => dispatch => {
-    dispatch({ type: DECREMENT });
+export const decrement = () => {
+    return { type: DECREMENT };
 };
