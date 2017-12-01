02ba66737ab2619ac0c2356a62ea54e51ff7eca3
Sako Hartounian <shartounian@DSC-ML-SHARTOUNIAN.local>
2017-11-30 18:11:18 -0800

    ReactHelmet added for SEO.

--- src/client/pages/About.js
+++ src/client/pages/About.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
-const About = () => <h1>About Us</h1>;
+const About = () => (
+    <div>
+        <Helmet>
+            <title>About Us</title>
+        </Helmet>
+        <h1>About Us</h1>
+    </div>
+);
 
 export default About;
