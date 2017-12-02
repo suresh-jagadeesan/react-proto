dcce056797c1495b28e9a725172d2c485ec10bb8
Sako Hartounian <shartounian@DSC-ML-SHARTOUNIAN.local>
2017-12-01 17:57:42 -0800

    actions created for fetching Users from REST API endpoints.

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,4 +1,11 @@
-import { INCREMENT, DECREMENT, RESET } from './action-types';
+import { 
+    INCREMENT, 
+    DECREMENT, 
+    RESET, 
+    FETCH_USERS,
+    FETCH_USERS_SUCCESS,
+    FETCH_USERS_FAILURE
+} from './action-types';
 
 export function increment () {
     return { type: INCREMENT };
@@ -11,3 +18,21 @@ export function decrement () {
 export function reset () {
     return { type: RESET };
 }
+
+export function fetchUsers() {
+    return { type: FETCH_USERS };
+}
+
+export function fetchUsersSuccess(users) {
+    return { 
+        type: FETCH_USERS_SUCCESS,
+        payload: { users } 
+    };
+};
+
+export function fetchUsersFailure(error = { message: 'unknown error' }) {
+    return { 
+        type: FETCH_USERS_FAILURE,
+        payload: { error } 
+    };
+};
--- test/actions/index.spec.js
+++ test/actions/index.spec.js
@@ -1,5 +1,19 @@
-import { INCREMENT, DECREMENT, RESET } from '../../src/client/actions/action-types';
-import { increment, decrement, reset } from '../../src/client/actions';
+import { 
+    INCREMENT, 
+    DECREMENT, 
+    RESET,
+    FETCH_USERS,
+    FETCH_USERS_SUCCESS,
+    FETCH_USERS_FAILURE 
+} from '../../src/client/actions/action-types';
+import { 
+    increment, 
+    decrement, 
+    reset,
+    fetchUsers,
+    fetchUsersSuccess,
+    fetchUsersFailure
+ } from '../../src/client/actions';
 
 
 
@@ -20,6 +34,39 @@ describe('actions', () => {
     describe('reset', () => {
         it('returns an action type of RESET', () => {
             expect(reset().type).toBe(RESET);
+        });
+    });
+
+    describe('fetchUsers', () => {
+        it('returns an action type of FETCH_USERS', () => {
+            expect(fetchUsers().type).toBe(FETCH_USERS);
+        });
+    });
+
+    describe('fetchUsersSuccess', () => {
+        it('returns an action type of FETCH_USERS', () => {
+            expect(fetchUsersSuccess().type).toBe(FETCH_USERS_SUCCESS);
+        });
+
+        it('accepts and returns a list of users', () => {
+            const users = [
+                { name: 'John', age: 30 },
+                { name: 'Jane', age: 20 }
+            ];
+
+            expect(fetchUsersSuccess(users).payload).toEqual({ users });
+        });
+    });
+
+    describe('fetchUsersFailure', () => {
+        it('returns an action type of FETCH_USERS', () => {
+            expect(fetchUsersFailure().type).toBe(FETCH_USERS_FAILURE);
+        });
+
+        it('should return an error message', () => {
+            const error = { message: 'Cannot get the list of users' };
+
+            expect(fetchUsersFailure(error).payload).toEqual({ error });
         });
     });
 });
